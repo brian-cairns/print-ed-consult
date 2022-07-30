f0adeb51d7f7e7200e7de205c235a3f1a5e0141d
brian-cairns <79775364+brian-cairns@users.noreply.github.com>
2022-07-30 16:25:40 -0400

    initial

---
+++ printForm.js
@@ -0,0 +1,68 @@
+const params = new URLSearchParams(window.location.search)
+for (const [key, value] of params) { let id = value; }
+
+//Show animation 
+document.getElementById('pageTitle').style.display = "none";
+document.getElementById('formBody').style.display = "none";
+document.getElementById('returnSection').style.display = "none";
+
+//fetch data
+const url = 'https://pffm.azurewebsites.net/getForms'
+const query = {
+    form: 'educationalConsultation',
+    itemId: id 
+}
+const header = {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin": "*"
+}
+
+fetch(url, {
+    method: "GET",
+    headers: header,
+    body: JSON.stringify(query)
+})
+    .then(response => response.json())
+    .then(data => populatePage(data))
+    .catch(console.error)
+
+async function populatePage(data) {
+    document.getElementById('staffEmail').innerHTML = data.employeeEmail;
+    document.getElementById('clientName').innerHTML = data.clientName;
+    document.getElementById('members').innerHTML = data.membersPresent;
+    document.getElementById('date').innerHTML = data.date;
+    document.getElementById('start').innerHTML = data.start;
+    document.getElementById('end').innerHTML = data.end;
+    document.getElementById('notes').innerHTML = data.notes;
+    document.getElementById('followUp').innerHTML = data.followUp;
+    document.getElementById('staffName').innerHTML = data.staffName;
+
+    //create arrays
+    concern = [data.parentalConcern1, data.parentalConcern2, data.parentalConcern3];
+    resolution = [data.resolution1, data.resolution2, data.resolution3];
+    nextSteps = [data.nextSteps1, data.nextSteps2, data.nextSteps3];
+    
+    //show arrays
+    for (let i = 1; i < 4; i++) {
+        document.getElementById(`concern${i}`).innerHTML = data.parentalConcerns[i];
+        document.getElementById(`resolution${i}`).innerHTML = data.resolution[i];
+        document.getElementById(`nextSteps${i}`).innerHTML = data.nextSteps[i];
+        if(!data.parentalConcerns[i+1]) {i=4}
+    }
+    showPage()
+}
+
+function showPage() {
+    document.getElementById('pageTitle').style.display = "block";
+    document.getElementById('formBody').style.display = "block";
+    document.getElementById('returnSection').style.display = "block";
+    document.getElementById('loadingAnimationSection').style.display = "none";
+}
+
+let printToPDF = document.getElementById('printToPDF')
+printToPDF.addEventListener('click', (e) = {})
+
+let exit = document.getElementById('exit') 
+exit.addEventListener('click', (e) => {
+    history.back()
+})
